4b7ce15fc352a4a9139fa1cac2462a09514ab010
manhloi5501 <manhloi0505@gmail.com>
2021-06-07 09:54:10 +0700

    change method login

--- src/modules/User/user.controller.js
+++ src/modules/User/user.controller.js
@@ -40,7 +40,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const account = req.body;
     // check account
